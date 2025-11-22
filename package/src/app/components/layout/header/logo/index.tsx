8e167f99864754b83db74b41936c8cd1d72ec0a2
afrocan1 <114925535+afrocan1@users.noreply.github.com>
2025-11-22 00:38:24 -0800

    Update Logo component dimensions

--- package/src/app/components/layout/header/logo/index.tsx
+++ package/src/app/components/layout/header/logo/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Logo: React.FC = () => {
   return (
     <Link href="/">
-      <div style={{ position: "relative", width: "160px", height: "50px" }}>
+      <div style={{ position: "relative", width: "220px", height: "65px" }}>
         <Image
           src="/images/logo/Untitled_design__5_-removebg-preview.png"
           alt="logo"
